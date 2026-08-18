27b8ed17ffaa5cc30e17d040a558e610e0f8fddc
FlumpterBumpkin <charednich@gmail.com>
2026-08-18 17:36:39 -0400

    Created script for demo in the video

---
+++ finalDemo01.m
@@ -0,0 +1,124 @@
+clear;
+clc;
+
+%% Load Data
+
+%Folder containing the four class subfolders
+dataDir = fullfile("full_set");
+
+assert(isfolder(dataDir), "Image folder not found: %s", dataDir);
+
+imds = imageDatastore(dataDir, "IncludeSubfolders",true, "LabelSource","foldernames");
+
+fprintf("Loaded %d image paths\n", numel(imds.Files));
+classCounts = countEachLabel(imds);
+disp(classCounts);
+
+fprintf("Sampling images...");
+sample = splitEachLabel(imds, 3, 'randomized');
+numImages = numel(sample.Files);
+
+networkName = "efficientnetb0"; %for printing convenience
+net = efficientnetb0;
+featureLayer = "efficientnet-b0|model|head|global_average_pooling2d|GlobAvgPool";
+
+inputSize = net.Layers(1).InputSize;
+
+fprintf("\nNetwork: %s\n",networkName);
+fprintf("Input size: %d x %d x %d\n",...
+    inputSize(1),...
+    inputSize(2),...
+    inputSize(3));
+fprintf("Feature layer: %s\n",featureLayer);
+
+img = readimage(sample,1);
+
+imgPrepared = prepareCNNImage(img, inputSize);
+
+activation = activations(net, imgPrepared, featureLayer);
+
+fprintf("\nRaw activation size:\n");
+disp(size(activation));
+
+featureVector = reshape(activation,1,[]);
+
+fprintf("Feature vector length:\n");
+disp(length(featureVector));
+
+
+%determine feature size
+featureLength = length(featureVector);
+
+X_CNN = zeros(numImages, featureLength, "single");
+
+for i = 1:numImages
+
+    img = readimage(sample,i);
+    imgPrepared = prepareCNNImage(img, inputSize);
+    activation = activations(net, imgPrepared, featureLayer);
+
+    %IMPORTANT:
+    %convert arbitrary CNN output into one feature row
+    X_CNN(i,:) = reshape(activation, 1,...
+        []);
+
+    if mod(i,100)==0
+        fprintf("Processed %d/%d images\n", i, numImages);
+    end
+end
+
+Y = sample.Labels;
+
+mdl = load("NN_EfficientnetB0FINAL02.mat", "nn");
+mdl = mdl.nn;
+gnames = load("gnames.mat", "gnames");
+gnames = gnames.gnames;
+
+[accuracy, label] = analyzeNN(mdl, X_CNN, Y, gnames);
+
+fprintf("Overall Accuracy: %f\n", accuracy);
+disp(label);
+for i = 1: numImages
+    imtool(readimage(sample, i));
+end
+
+% Analyze
+function [accuracy, label] = analyzeNN(mdl, xValid, yValid, gnames)
+[predicted, score] = predict(mdl, xValid);
+predicted = gnames(gather(predicted));
+trueLabels = cellstr(gather(yValid));
+isCorrect = strcmp(predicted, trueLabels);
+accuracy = sum(isCorrect, "all") / numel(yValid);
+label = [(num2cell(1:numel(yValid))'), predicted(:), num2cell(isCorrect(:))];
+end
+
+%% Prepare Image Function
+%Similar to how images are processed for HOG
+function output = prepareCNNImage(img,inputSize)
+
+%convert grayscale images if necessary
+if size(img,3)==1
+    img = cat(3,img,img,img);
+end
+
+% Current strategy:
+%   Resize image while preserving aspect ratio
+%   Then pad to CNN input size
+% This avoids stretching insects.
+
+targetH = inputSize(1);
+targetW = inputSize(2);
+
+scale = min(targetH/size(img,1), targetW/size(img,2));
+newSize = round([size(img,1),size(img,2)]*scale);
+img = imresize(img,newSize);
+
+%create padded image
+output = zeros(targetH, targetW, 3, "like", img);
+
+rowStart = floor((targetH-newSize(1))/2)+1;
+colStart = floor((targetW-newSize(2))/2)+1;
+
+output(rowStart:rowStart+newSize(1)-1, colStart:colStart+newSize(2)-1, :) = img;
+
+end
